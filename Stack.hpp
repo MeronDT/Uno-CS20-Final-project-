dc18a81c616e758e162b85f02adafc8f301205b7
MeronDT <111631008+MeronDT@users.noreply.github.com>
2022-12-19 11:00:07 -0800

    Create Stack.hpp
    copy and pasted

---
+++ Stack.hpp
@@ -0,0 +1,32 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+template <typename T>
+
+//Stack class used as parent for other classes
+
+class Stack {
+    protected:
+        //the current number of elements in the stack
+        int length;
+
+    public:
+        //default constructor
+        Stack() : length(0) { }
+        //destructor
+        virtual ~Stack() { }
+        //remove all elements in the stack, resetting to the initial state
+        virtual void clear() = 0;
+        //return the current length of the stack
+        virtual int getLength() const = 0;
+        //determine if the stack is currently empty
+        virtual bool isEmpty() const = 0;
+        //return the element at the top of the stack
+        virtual T peek() const = 0;
+        //remove the top element from the stack
+        virtual void pop() = 0;
+        //add the argument to the top of the stack
+        virtual void push(const T&) = 0;
+};
+
+#endif
